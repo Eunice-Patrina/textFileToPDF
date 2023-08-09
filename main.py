128cb05bdb68b112e00e3c0da50801173cf65eb3
Eunice <eunicepatrina0@gmail.com>
2023-08-09 14:43:24 +0530

    Display content of files in PDF

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from fpdf import FPDF
 import glob
 from pathlib import Path
@@ -17,6 +16,15 @@ for filepath in filepaths:
     pdf.add_page()
     pdf.set_font(family="Times", style="B", size=16)
     pdf.cell(w=0, h=12, txt=filename.capitalize(), ln=1)
+
+    # get text from the file
+    with open(filepath, 'r') as file:
+        content = file.read()
+
+    # display file content in pdf
+    pdf.set_font(family="Times", size=12)
+    pdf.multi_cell(w=0, h=8, txt=content)
+
 
 pdf.output("output.pdf")
 
